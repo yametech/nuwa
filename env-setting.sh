37d9a18489b1c47082ba595d3397e69d19ea6300
laik <laik.lj@me.com>
2020-03-23 17:10:47 +0800

    env-setting incr developer convenience

--- env-setting.sh
+++ env-setting.sh
@@ -2,7 +2,7 @@
 # Development environment Dns $(SERVICE_NAME).$(NAMESPACE_NAME).svc.cluster.local
 # Image registry IP address 192.168.100.1
 
-export DEPLOYMENT_DNSNAME=nuwa-controller-manager-service.svc.cluster.local
+export NUWA_DEV_IP=10.1.140.175
 export COUNTRY=CN
 export CITY=GuangZhou
 export IMG=yametech/nuwa-controller:latest
